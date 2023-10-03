413e45e6b55323ca5d4337589ac1469d73b9abf2
git4alauddin <alauddin4edu@gmail.com>
2023-10-03 14:47:59 +0530

    add class to form for customization

--- forms/auth_form.py
+++ forms/auth_form.py
@@ -6,9 +6,9 @@ from models.user_model import User
 
 # form register
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[input_required(), Length(min=4, max=20)], render_kw={'placeholder': 'Username'})
-    email = EmailField('Email', validators=[input_required(), Email()], render_kw={'placeholder': 'Email'})
-    password = PasswordField('Password', validators=[input_required(), Length(min=4, max=20)], render_kw={'placeholder': 'Password'})
+    username = StringField('Username', validators=[input_required(), Length(min=4, max=20)], render_kw={'placeholder': 'Username', 'class': 'form-control'})
+    email = EmailField('Email', validators=[input_required(), Email()], render_kw={'placeholder': 'Email', 'class': 'form-control'})
+    password = PasswordField('Password', validators=[input_required(), Length(min=4, max=20)], render_kw={'placeholder': 'Password', 'class': 'form-control'})
     submit = SubmitField('Register')  
     
 # form login
