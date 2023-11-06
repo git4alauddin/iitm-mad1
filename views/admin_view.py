203401331e4c19524f54bc6ea5860b399e50cbaa
git4alauddin <alauddin4edu@gmail.com>
2023-11-07 04:55:56 +0530

    added creator stats and admin stats with admin-stats graph

--- views/admin_view.py
+++ views/admin_view.py
@@ -1,19 +1,28 @@
 from flask import Blueprint, render_template,redirect,url_for,flash,request
 from flask.views import MethodView
 import requests
+from models.music_model import Song,Album,Playlist
+from models.user_model import User
 
 # --------------------------------------blueprint admin--------------------------------------------------------
 bp_admin = Blueprint('admin', __name__)
 
-class Users(MethodView):
+# view admin_visuals
+class AdminVisualsView(MethodView):
     def get(self):
-        api_url = request.url_root + 'users/users'
-        response = requests.get(api_url)
+        # stats
+        tot_user = User.query.filter_by(role='user').count()
+        tot_creator = User.query.filter_by(role='creator').count()
+        tot_album = Album.query.count()
+        tot_song = Song.query.count()
+        tot_playlist = Playlist.query.count()
 
-        if response.status_code == 200:
-            users = response.json()
-            return render_template('users.html', users=users)
-        else:
-            flash('Failed to fetch users', 'danger')
-            return redirect(url_for('user.dashboard'))
-bp_admin.add_url_rule('/users', view_func=Users.as_view('users'))
+        stats_headings = ['Total Normal Users', 'Total Creators', 'Total Albums', 'Total Songs', 'Total Playlists']
+        stats_data = [{'heading': h, 'total': t} for h, t in zip(stats_headings, [tot_user, tot_creator, tot_album, tot_song, tot_playlist])]
+
+        labels = ['Total Normal Users', 'Total Creators', 'Total Albums', 'Total Songs', 'Total Playlists']
+        values = [tot_user, tot_creator, tot_album, tot_song, tot_playlist]
+        return render_template('admin_visuals.html', labels=labels, values=values, stats_data=stats_data)
+bp_admin.add_url_rule('/admin_visuals', view_func=AdminVisualsView.as_view('admin_visuals'))
+
+
--- views/user_view.py
+++ views/user_view.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template,redirect,url_for,flash,request
 from flask_login import logout_user,login_required, current_user
 from flask.views import MethodView
 import requests
-from models.music_model import Playlist
+from models.music_model import Playlist, Song, Album
 
 #------------------------------------blueprint user---------------------------------------#
 bp_user = Blueprint('user', __name__)
@@ -17,13 +17,15 @@ class DashboardView(MethodView):
         s_response = requests.get(api_url)
         api_url = request.url_root + 'users/users/' + str(current_user.id) + '/albums'
         a_response = requests.get(api_url)
-        
+            
         if s_response.status_code == 200:
             suggested_songs = s_response.json()
         if p_response.status_code == 200:
             playlists = p_response.json()
         if a_response.status_code == 200:
             albums = a_response.json()
+
+
         return render_template('dashboard.html', playlists=playlists, suggested_songs=suggested_songs, albums=albums)
 bp_user.add_url_rule('/dashboard', view_func=DashboardView.as_view('dashboard'))
 
@@ -48,3 +50,33 @@ class CreatorRegisterView(MethodView):
             flash('Error registering as creator!', 'danger')
             return redirect(url_for('user.dashboard'))
 bp_user.add_url_rule('/register/creator', view_func=CreatorRegisterView.as_view('creator_register'))
+
+# view creator_stats
+class CreatorStatsView(MethodView):
+    def get(self):
+        #contents
+        api_url = request.url_root + 'users/users/' + str(current_user.id) + '/playlists'
+        p_response = requests.get(api_url)
+        api_url = request.url_root + 'songs/songs'
+        s_response = requests.get(api_url)
+        api_url = request.url_root + 'users/users/' + str(current_user.id) + '/albums'
+        a_response = requests.get(api_url)
+        
+        if s_response.status_code == 200:
+            suggested_songs = s_response.json()
+        if p_response.status_code == 200:
+            playlists = p_response.json()
+        if a_response.status_code == 200:
+            albums = a_response.json()
+
+        # stats
+        stats_headings = ['Total Albums', 'Total Songs', 'Total Playlists', 'Average Rating']
+        tot_song = Song.query.filter_by(creator_id=current_user.id).count()
+        tot_album = Album.query.filter_by(user_id=current_user.id).count()
+        tot_playlist = Playlist.query.filter_by(user_id=current_user.id).count()
+        average_rating = 3.4
+
+        stats_data = [{'heading': h, 'total': t} for h, t in zip(stats_headings, [tot_album, tot_song, tot_playlist, average_rating])]
+
+        return render_template('creator_stats.html', playlists=playlists, suggested_songs=suggested_songs, albums=albums, stats_data=stats_data)
+bp_user.add_url_rule('/creator_stats', view_func=CreatorStatsView.as_view('creator_stats'))
